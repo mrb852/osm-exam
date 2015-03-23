4d9768ace3d95ec1b90705e45b33af2f309df1d3
Christian Enevoldsen <chrene91@gmail.com>
2015-03-23 20:26:59 +0100

    Adds comments to rand.c test program

--- buenos/tests/rand.c
+++ buenos/tests/rand.c
@@ -1,9 +1,20 @@
+/**
+ * This program tests the functionallity of the pseudo random
+ * syscall implementation.
+ */
+
 #include "tests/lib.h"
 
 int main() {
-  int i = 0;
+
+  // Gets random numbers with range from 0 to 40
+  int i, r = 0;
   for (; i < 40; ++i) {
-    printf("%d\n", syscall_rand(i));
+    r = syscall_rand(i);
+    printf("syscall_rand(%d): %d\n", i, r);
+    if (r >= i) {
+      printf("ERROR: random is over range.");
+    }
   }
 
   return 0;
